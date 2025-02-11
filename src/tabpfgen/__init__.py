efadc995be89ac4755108dcc6943e85c9474af56
Seb Haan <sebhaan@gmail.com>
2025-02-11 15:37:54 +1100

    Add init file

---
+++ src/tabpfgen/__init__.py
@@ -0,0 +1,12 @@
+from .generator import TabPFGen
+from .visuals import (
+    visualize_classification_results,
+    visualize_regression_results
+)
+
+__version__ = "0.1.0"
+__all__ = [
+    "TabPFGen",
+    "visualize_classification_results",
+    "visualize_regression_results"
+]
