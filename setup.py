66d5553f6425d0d9484ce73f1c1bb89c11d4663c
Seb Haan <sebhaan@gmail.com>
2025-02-11 16:06:25 +1100

    simplify pyproject and update setup.py

--- setup.py
+++ setup.py
@@ -18,11 +18,16 @@ setup(
     packages=find_packages(where="src"),
     python_requires=">=3.11",
     install_requires=read_requirements('requirements.txt'),
+    extras_require={
+        'dev': read_requirements('requirements-dev.txt'),
+        'docs': read_requirements('requirements-docs.txt'),
+    },
     classifiers=[
-        "License :: OSI Approved :: Apache-2.0 License",
+        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     include_package_data=True,
 )
